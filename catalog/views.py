501ece5a8aabd8436076a81675cb7d19f5c8e861
Juraj Vida <zoldo.vida@gmail.com>
2021-04-15 00:16:58 +0200

    Security measures

--- catalog/views.py
+++ catalog/views.py
@@ -341,8 +341,8 @@ def gallery(request):
 
     context = {}
     # files = os.listdir(os.path.join(settings.STATIC_ROOT, "img/gallery/"))
-    gallery_images = os.listdir('catalog/static/img/gallery/')   # TODO toto mozno este nejako upravit rozumnejsie
-    #gallery_images = os.listdir('/home/drozdo/safari/catalog/static/img/gallery/')
+    # gallery_images = os.listdir('catalog/static/img/gallery/')   # TODO toto mozno este nejako upravit rozumnejsie
+    gallery_images = os.listdir('/home/drozdo/safari/catalog/static/img/gallery/')
     context['images'] = gallery_images
 
     # Render the HTML template catalog/gallery.html with the data in the context variable
